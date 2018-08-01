4c394b81cf2c7d9b9eeaa3002cccd451d201fee7
David Kolia <Dkolia@icloud.com>
2018-08-01 12:20:03 -0500

    Created renders to screen, magnitude and location

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 import Quakes from "./Quakes";
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {quakeData: []}
+  }
 
+  getQuakes = async () => {
+    try {
+      const quakesFound = fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson")
+    } catch (err) {
+      return err
+    }
+  }
 
   render() {
     return (
